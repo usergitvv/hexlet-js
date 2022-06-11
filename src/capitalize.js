6842cbbf1bcf76f2f50175677969ab406ee83406
usergitvv <valery.voronkov@yandex.ru>
2022-06-11 15:43:43 +0500

    New modifications - add assert in package.json, using assert in test-file, special mistake in src/capitalize.js for test's check

--- src/capitalize.js
+++ src/capitalize.js
@@ -2,10 +2,12 @@ const capitalize = (text) => {
     if (text === '') {
       return '';
     }
+    // if (typeof text !== 'string') return 'Неправильный тип введённых данных!';
     const firstChar = text[0].toUpperCase();
-    const restSubstring = text.slice(1);
+    const restSubstring = text.slice(2);
     return `${firstChar}${restSubstring}`;
   };
 
+  capitalize('hello');
+
   export default capitalize;
-  
--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,8 @@
 import capitalize from "../src/capitalize.js";
+import {strict as assert} from 'assert';
 
-if (capitalize('hello') !== 'Hello') {
-    throw new Error('Функция работает неверно!');
-}
-  
-  if (capitalize('') !== '') {
-    throw new Error('Функция работает неверно!');
-}
-  
-console.log('Все тесты пройдены!');
+assert.equal(capitalize('hello'), 'Hello');
+assert.equal(capitalize(''), '', new TypeError('Функция работает неверно!'));
+
+ 
+// console.log('Все тесты пройдены!');
